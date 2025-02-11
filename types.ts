a5a8672de72460d2d424a7457060e3ca8600bdba
Sebas <sebasrojash08@gmail.com>
2025-02-11 11:34:33 -0600

    objCreation

--- types.ts
+++ types.ts
@@ -21,3 +21,26 @@ list.forEach(function val(){
 list.forEach((val) => {
     console.log(val)
 });
+
+function printobj(ob: {ob1: string | number, ob2?: number}){ //el simbolo ? significa que el parametro seleccionado es opcional
+    if (ob.ob2 != undefined) {
+        console.log("Los objetos establecidos son " + ob.ob1, " ",  ob.ob2)
+    } else{
+        console.log("El parametro es: ", ob.ob1)
+    }
+    
+}
+printobj({ob1: "sebas"})
+
+
+//typeof
+
+function verify(type1: string | boolean){
+    if (typeof type1 === "boolean"){
+        async function dbTrue(param:string): Promise<number> {
+            return 1;
+        }
+    }
+}
+
+
