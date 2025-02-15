fc0afb93ce9e85f6fdb073c330da351de1bbe88b
Sebas <sebasrojash08@gmail.com>
2025-02-15 15:56:26 -0600

    newFile-async

---
+++ asyncFunctns.ts
@@ -0,0 +1,9 @@
+const n: number = obj;
+
+async function test(): Promise<number>{ //una promesa es una función de la cual el programa no necesariamente espera que se cumpla para seguir en funcionamiento 
+    return 1
+        //setTimeOut (por lo general se establece con tipos de datos de acuerdo a la solicitud y seguidamente un tiempo de espera en ms)
+        //resolve (condición verdadera)
+        //reject (falsa)
+        //then 
+}
--- types.ts
+++ types.ts
@@ -3,15 +3,6 @@ let obj: any = {
     c: "donald"
 }
 
-const n: number = obj;
-
-async function test(): Promise<number>{ //una promesa es una función de la cual el programa no necesariamente espera que se cumpla para seguir en funcionamiento 
-    return 1
-        //setTimeOut (por lo general se establece con tipos de datos de acuerdo a la solicitud y seguidamente un tiempo de espera en ms)
-        //resolve (condición verdadera)
-        //reject (falsa)
-        //then 
-}
 const list = [1, 2, 3];
 
 list.forEach(function val(){
