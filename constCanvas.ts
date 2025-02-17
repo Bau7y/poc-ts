d12db864930a90b17350059251f78bf3af916944
Sebas <sebasrojash08@gmail.com>
2025-02-16 23:09:05 -0600

    handleOfReq

--- constCanvas.ts
+++ constCanvas.ts
@@ -1,2 +1,8 @@
 const myCanvas = document.getElementById("main_canvas") as HTMLCanvasElement;
 const myCanvas2 = <HTMLCanvasElement>document.getElementById("main_canvas");
+
+
+declare function handleRequest(url: string, method: "GET" | "POST"): void;
+const req = { url: "example.org", method: "GET" };
+handleRequest(req.url, req.method as "GET");
+
