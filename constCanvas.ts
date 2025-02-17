b5d8ed2c7af3d84396e06873d482f0000d454e5b
Sebas <sebasrojash08@gmail.com>
2025-02-17 15:22:20 -0600

    startingNarrowing

--- constCanvas.ts
+++ constCanvas.ts
@@ -4,5 +4,5 @@ const myCanvas2 = <HTMLCanvasElement>document.getElementById("main_canvas");
 
 declare function handleRequest(url: string, method: "GET" | "POST"): void;
 const req = { url: "example.org", method: "GET" };
-handleRequest(req.url, req.method as "GET");
+handleRequest(req.url, req.method as "POST");
 
