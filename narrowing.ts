206d39e1d00b206338b336d286d2ccb9d305866a
Sebas <sebasrojash08@gmail.com>
2025-02-18 13:36:41 -0600

    firstForNMapUse

--- narrowing.ts
+++ narrowing.ts
@@ -19,10 +19,19 @@ function multiply(
     values: number[] | undefined,
     factor: number
 ): number[] | undefined{
-    if (!values){
-        return values
-    } else {
-        return values.map((x) => x * factor);
+    if (!values) return values; //Basicamente si esto se cumple termina la funcion
+    return values.map((x) => x * factor); //Esta linea solamente se reaaliza si el if no se cumple (Es como un else) y el .map, crea una matriz en base a la orden que se le indique 
+}
+
+function printAll2(strs: string | string[] | null) {
+    if (strs !== null) {
+        if (typeof strs === "object") {
+            for (const s of strs) {
+                console.log(s);
+            }
+        } else if (typeof strs === "string") {
+            console.log(strs);
+        }
     }
 }
 
